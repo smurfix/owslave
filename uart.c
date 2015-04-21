e6b2c5533d8c4c3f1a792b7d1271171c3fbdce99
Matthias Urlichs <matthias@urlichs.de>
2015-04-21 05:55:22 +0200

    reduce default UART buffers

--- uart.c
+++ uart.c
@@ -75,11 +75,11 @@ LICENSE:
 
 /** Size of the circular receive buffer, must be power of 2 */
 #ifndef UART_RX_BUFFER_SIZE
-#define UART_RX_BUFFER_SIZE 32
+#define UART_RX_BUFFER_SIZE 8
 #endif
 /** Size of the circular transmit buffer, must be power of 2 */
 #ifndef UART_TX_BUFFER_SIZE
-#define UART_TX_BUFFER_SIZE 256
+#define UART_TX_BUFFER_SIZE 128
 #endif
 
 
