be50991a63df0640ba120b6bb3b695c3cc2a3b8a
Matthias Urlichs <matthias@urlichs.de>
2015-04-22 17:00:21 +0200

    missing definitions

--- debug.h
+++ debug.h
@@ -19,7 +19,7 @@
 #include "dev_config.h"
 
 /* Debugging */
-#ifdef HAVE_UART
+#if defined(HAVE_UART) && !defined(NO_DEBUG)
 #include "uart.h"
 
 /* These macros always send something. They should only be used in
@@ -50,6 +50,7 @@
 
 #ifndef DBG_C
 #define DBG_C(x) do { } while(0)
+#define DBG_P(x) do { } while(0)
 #define DBG_P_(x) do { } while(0)
 #define DBG_N(x) do { } while(0)
 #define DBG_W(x) do { } while(0)
@@ -59,6 +60,7 @@
 
 #ifndef DBGS_C
 #define DBGS_C(x) do { } while(0)
+#define DBGS_P(x) do { } while(0)
 #define DBGS_P_(x) do { } while(0)
 #define DBGS_N(x) do { } while(0)
 #define DBGS_W(x) do { } while(0)
@@ -70,8 +72,7 @@
 #define DBG_TS() do { } while(0)
 #endif
 
-#ifdef HAVE_DBG_PORT
-EXTERN volatile char dbg_interest INIT(0);
+#if defined(HAVE_DBG_PORT) && !defined(NO_DEBUG)
 #define DBGA(x,v) do { v=(x); asm volatile("out %0,%1" :: "i"(((int)&DBGPORT)-__SFR_OFFSET),"r"(v)); } while(0)
 #define DBG(x) do { uint8_t _x; DBGA(x,_x); } while(0)
 #else
@@ -79,7 +80,7 @@ EXTERN volatile char dbg_interest INIT(0);
 #define DBGA(x,v) do { } while(0)
 #endif
 
-#ifdef HAVE_DBG_PIN
+#if defined(HAVE_DBG_PIN) && !defined(NO_DEBUG)
 #define DBG_ON() do { DBGPINPORT |= (1<<DBGPIN); } while(0)
 #define DBG_OFF() do { DBGPINPORT &= ~(1<<DBGPIN); } while(0)
 #else
