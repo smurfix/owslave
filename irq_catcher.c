b3014b18ad6469b453adb020999e15ad06cf0cbf
Matthias Urlichs <matthias@urlichs.de>
2015-07-27 16:16:53 +0200

    Make irq_catcher footprint way smaller, report IRQ

--- irq_catcher.c
+++ irq_catcher.c
@@ -18,57 +18,72 @@
  * to the interrupt which would otherwise have reset your IC because
  * you forgot to implement a handler for it.
  */
-#include "onewire.h"
 #include "features.h"
-#include "debug.h"
 
 #ifdef HAVE_IRQ_CATCHER
 
+#include "debug.h"
+#include "uart.h"
+#include <avr/wdt.h>
+
+uint8_t reboot_irq __attribute__ ((section (".noinit")));
+
 void ping_me(uint8_t c) {
-    uint32_t j;
+	asm("eor r1,r1"); // we don't know where we came from
+	cli();
     DBG_OFF();DBG_ON();DBG_OFF();DBG_ON();DBG_OFF();
     DBG(c);
     DBG_ON();DBG_OFF(); DBG_ON();DBG_OFF();
-    for(j=0;j<1000000;j++) ;
-
-    extern void __ctors_end(void); __ctors_end();
+#if defined(HAVE_WATCHDOG)
+	wdt_reset();
+#endif
+#ifdef HAVE_UART_SYNC
+	uart_puts_P("\nDEAD x");
+	uart_puthex_byte(c);
+	uart_putc('\n');
+	uart_putc(' ');
+#endif
+	wdt_enable(0); // (almost) immediate
+    do {} while(1);
 }
 
-void __vector_1(void) __attribute__((signal));
-void __vector_2(void) __attribute__((signal));
-void __vector_3(void) __attribute__((signal));
-void __vector_4(void) __attribute__((signal));
-void __vector_5(void) __attribute__((signal));
-void __vector_6(void) __attribute__((signal));
-void __vector_7(void) __attribute__((signal));
-void __vector_8(void) __attribute__((signal));
-void __vector_9(void) __attribute__((signal));
-void __vector_10(void) __attribute__((signal));
-void __vector_11(void) __attribute__((signal));
-void __vector_12(void) __attribute__((signal));
-void __vector_13(void) __attribute__((signal));
-void __vector_14(void) __attribute__((signal));
-void __vector_15(void) __attribute__((signal));
-void __vector_16(void) __attribute__((signal));
-void __vector_17(void) __attribute__((signal));
-void __vector_18(void) __attribute__((signal));
-void __vector_19(void) __attribute__((signal));
-void __vector_20(void) __attribute__((signal));
-void __vector_21(void) __attribute__((signal));
-void __vector_22(void) __attribute__((signal));
-void __vector_23(void) __attribute__((signal));
-void __vector_24(void) __attribute__((signal));
-void __vector_25(void) __attribute__((signal));
-void __vector_26(void) __attribute__((signal));
-void __vector_27(void) __attribute__((signal));
-void __vector_28(void) __attribute__((signal));
-void __vector_29(void) __attribute__((signal));
-void __vector_30(void) __attribute__((signal));
-void __vector_31(void) __attribute__((signal));
+// These reset anyway, so don't bother with niceties
+void __vector_1(void) __attribute__((naked));
+void __vector_2(void) __attribute__((naked));
+void __vector_3(void) __attribute__((naked));
+void __vector_4(void) __attribute__((naked));
+void __vector_5(void) __attribute__((naked));
+void __vector_6(void) __attribute__((naked));
+void __vector_7(void) __attribute__((naked));
+void __vector_8(void) __attribute__((naked));
+void __vector_9(void) __attribute__((naked));
+void __vector_10(void) __attribute__((naked));
+void __vector_11(void) __attribute__((naked));
+void __vector_12(void) __attribute__((naked));
+void __vector_13(void) __attribute__((naked));
+void __vector_14(void) __attribute__((naked));
+void __vector_15(void) __attribute__((naked));
+void __vector_16(void) __attribute__((naked));
+void __vector_17(void) __attribute__((naked));
+void __vector_18(void) __attribute__((naked));
+void __vector_19(void) __attribute__((naked));
+void __vector_20(void) __attribute__((naked));
+void __vector_21(void) __attribute__((naked));
+void __vector_22(void) __attribute__((naked));
+void __vector_23(void) __attribute__((naked));
+void __vector_24(void) __attribute__((naked));
+void __vector_25(void) __attribute__((naked));
+void __vector_26(void) __attribute__((naked));
+void __vector_27(void) __attribute__((naked));
+void __vector_28(void) __attribute__((naked));
+void __vector_29(void) __attribute__((naked));
+void __vector_30(void) __attribute__((naked));
+void __vector_31(void) __attribute__((naked));
 
-#ifdef HAVE_TIMER
+#if defined(HAVE_TIMER) && !defined(HAVE_TOV0)
 #define HAVE_TOV0
 #endif
+
 #ifdef HAVE_ONEWIRE
 #if defined (__AVR_ATmega168__) || defined (__AVR_ATmega88__) || defined(__AVR_ATmega328__)
 #define HAVE_TOV2
@@ -76,7 +91,7 @@ void __vector_31(void) __attribute__((signal));
 #define HAVE_TOV0
 #endif
 #else
-void __vector_1(void) { ping_me(1); }
+void __vector_1(void) { ping_me(1); } // INT0
 #endif
 void __vector_2(void) { ping_me(2); }
 void __vector_3(void) { ping_me(3); }
